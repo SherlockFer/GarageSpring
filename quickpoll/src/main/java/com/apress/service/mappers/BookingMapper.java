87cbac869f449bb1225deb88159cae4a78d73493
Sherlock <user@Sherlock.local>
2020-07-02 17:03:28 +0100

    first commit to Garage Spring

---
+++ quickpoll/src/main/java/com/apress/service/mappers/BookingMapper.java
@@ -0,0 +1,19 @@
+package com.apress.service.mappers;
+
+import java.util.Collection;
+
+import org.mapstruct.Mapper;
+
+import com.apress.domain.Booking;
+import com.apress.dto.BookingDTO;
+
+@Mapper(componentModel = "spring")
+public interface BookingMapper {
+
+	public BookingDTO toBookingDTO(Booking booking);
+
+	public Booking toBooking(BookingDTO bookingDTO);
+
+	public Collection<BookingDTO> toBookingDTOs(Collection<Booking> bookings);
+
+}
